03fe00fc47386d27d4d740d023a88e13931a6c27
Jacobo <jacob.et.cetera@gmail.com>
2017-08-10 18:28:33 -0500

    Imports

---
+++ Semana01/CallReference.java
@@ -0,0 +1,8 @@
+import static edu.itesm.mx.Referencia.*;
+/*Importación de static*/
+public class CallReference{
+	static int d = DAY;
+	CallReference(){
+		metodoEstatico();
+	}
+}
---
+++ Semana01/Referencia.java
@@ -0,0 +1,13 @@
+package edu.itesm.mx;
+import java.sql.*;
+public class Referencia{
+
+	java.util.Date date; //referencia del fully qualified import
+	Date sDate; //java.sql
+
+	public static int DAY = 100;
+
+	public static void metodoEstatico( ){
+
+	} 
+}
