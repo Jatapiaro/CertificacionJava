81affdd43b826cb1dbbc57c959c4ed74b55a980a
Jacobo <jacob.et.cetera@gmail.com>
2017-08-21 18:09:01 -0500

    ADdd

---
+++ Semana03/Clase01/Wrapper.java
@@ -0,0 +1,23 @@
+public class Wrapper{
+    public static void main(String[] args) {
+        int a = 10;
+        a = a++ + a + a--;
+        System.out.println(a);
+
+        Boolean bool6 = Boolean.valueOf("TrUE");
+
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println(Byte.MIN_VALUE);
+
+        System.out.println(bool6);
+        Boolean bool1 = true;
+        boolean b = bool1;
+        Character char1 = 'a';
+        char c = char1;
+        Byte byte1 = 10;
+        byte bt = byte1;
+        Double double1 = 10.98;
+        double dd = double1;
+
+    }
+}
---
+++ Semana03/Clase01/Wrapper2.java
@@ -0,0 +1,14 @@
+public class Wrapper2{
+    public static void main(String[] args) {
+
+        Integer i1 = new Integer(10);
+        Integer i2 = new Integer(10);
+        Integer i3 = Integer.valueOf(10);
+        Integer i4 = Integer.valueOf(10);
+        Integer i5 = 10;
+        Integer i6 = 10;
+
+        System.out.println();
+
+    }
+}
