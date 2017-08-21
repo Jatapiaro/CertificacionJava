23c878cbf65b8b756ab6f5081d77b7920700713a
Jacobo <jacob.et.cetera@gmail.com>
2017-08-21 18:09:20 -0500

    ADdd

--- Semana03/Clase01/Wrapper2.java
+++ Semana03/Clase01/Wrapper2.java
@@ -1,8 +1,11 @@
 public class Wrapper2{
     public static void main(String[] args) {
 
-        Integer i1 = new Integer(10);
+        Integer i1 = new Integer(10); //creates new instance
         Integer i2 = new Integer(10);
+        /*
+        * Use cache
+        */
         Integer i3 = Integer.valueOf(10);
         Integer i4 = Integer.valueOf(10);
         Integer i5 = 10;
